4fcbabbba2b036bfd269240eecd936a48b44eef1
gruvw <gruvw@users.noreply.github.com>
2024-09-02 12:02:08 +0000

    jekyll build from Action 356626add9a495d2d59307bad93496f2e53720e6

---
+++ assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,36 @@
+var store = [{
+        "title": "Designing my own watch face",
+        "excerpt":"I designed my own smart watch home screen and customized it the way I wanted it to be. 💎 The Desire I just bought the Xiaomi Band 4 which was the very new smart watch model from Xiaomi at the time. As I already had bought the Xiaomi Band 2...","categories": [],
+        "tags": ["Side Project"],
+        "url": "https://gruvw.com/2019/07/31/own-smart-watch-home-screen.html"
+      },{
+        "title": "AI in video games",
+        "excerpt":"I programmed a computer to learn how to play the game Snake on its own. ⭐ A High-School Project In my high school, I needed to create a graded project that I would submitted during my last year. We could choose any project we wanted but I already knew that...","categories": [],
+        "tags": ["Python"],
+        "url": "https://gruvw.com/2019/10/25/artificial-intelligence-in-video-games.html"
+      },{
+        "title": "Creating a python package",
+        "excerpt":"I created my first Python package.It keeps track of what I am printing to the console in a log file along with the possibility to add a prefix to what I am printing. 💡 The Idea As I coded multiple python programs that were running on a Raspberry Pi all...","categories": [],
+        "tags": ["Python"],
+        "url": "https://gruvw.com/2020/04/10/creating-a-python-package.html"
+      },{
+        "title": "This website 💻",
+        "excerpt":"In july 2020, I decided to build a website where I would be able to publish my different projects related to programming. That led to this website hosted on GitHub (powered by GitHub Pages and Jekyll). 📡 The Concept I had the idea of creating this kind of website a...","categories": [],
+        "tags": ["Web"],
+        "url": "https://gruvw.com/2020/07/01/this-website.html"
+      },{
+        "title": "Laser gun projected target",
+        "excerpt":"I designed and coded a virtual target projected on a wall in order to practice shooting at home using a laser gun. 💼 My First Coding Job I already had a job planned for the summer 2020, but with the COVID-19 pandemic it could not take place.I was left without...","categories": [],
+        "tags": ["Python","Hardware"],
+        "url": "https://gruvw.com/2020/09/01/laser-gun-projected-target.html"
+      },{
+        "title": "LaTeX writing setup",
+        "excerpt":"I decided to take notes during lectures with LaTeX and this is how I set up my LaTeX writing environment. 🗂️ Environment decision I went through a lot of different writing environments before settling with LaTeX on VSCode. Markdown note taking Before switching to LaTeX, I was taking notes in...","categories": [],
+        "tags": ["Side Project"],
+        "url": "https://gruvw.com/2021/05/17/latex-writing-setup.html"
+      },{
+        "title": "2D Plotter Robot",
+        "excerpt":"Most of the project is already explained on the README of the project on GitHub. 🧠 Concept I took a course at EPFL that taught students about the two sides of building things: the hardware part and the integration with software. During this course, I learned to use a laser...","categories": [],
+        "tags": ["C","Hardware"],
+        "url": "https://gruvw.com/2024/04/01/2d-plotter-robot.html"
+      }]
